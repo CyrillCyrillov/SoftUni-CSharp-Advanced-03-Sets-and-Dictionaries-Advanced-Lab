67dc4425eb1282922cb245643a9e85d70bfc70f4
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-29 03:12:46 +0200

    Add files via upload

---
+++ Task02_Average_Student_Grades/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task02_Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+            for (int i = 1; i <= numbers; i++)
+            {
+                string[] nextLineInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name = nextLineInfo[0];
+                decimal grade = decimal.Parse(nextLineInfo[1]);
+
+                if(students.ContainsKey(name))
+                {
+                    students[name].Add(grade);
+                }
+                else
+                {
+                    students.Add(name, new List<decimal> { grade });
+                }
+            }
+
+            foreach (var studentName in students)
+            {
+                Console.Write($"{studentName.Key} -> ");
+                foreach (var grades in studentName.Value)
+                {
+                    Console.Write($"{grades:f2} ");
+                }
+                Console.WriteLine($"(avg: {studentName.Value.Average():f2})");
+                
+                
+
+            }
+        }
+    }
+}

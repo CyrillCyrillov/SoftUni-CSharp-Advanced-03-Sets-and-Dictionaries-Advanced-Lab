4df7c048892edf49d07e6ed3f94baf8dc7c174e3
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-29 03:12:05 +0200

    Add files via upload

---
+++ Task01_Count_Same_Values_in_Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Sets_and_Dictionaries_Advanced_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputNumbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            Dictionary<double, int> counters = new Dictionary<double, int>();
+
+            foreach (double element in inputNumbers)
+            {
+                if (counters.ContainsKey(element))
+                {
+                    counters[element]++;
+                }
+                else
+                {
+                    counters.Add(element, 1);
+                }
+            }
+
+            foreach (var element in counters)
+            {
+                Console.WriteLine($"{element.Key} - {element.Value} times");
+            }
+        }
+    }
+}

a74828a688cce8e8c1e29e7b72e7b5d862e96e3f
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-29 03:15:09 +0200

    Add files via upload

---
+++ Task05_Record_Unique_Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task05_Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int numbers = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numbers; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
